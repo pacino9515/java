a585dbf16734ae891df1c90cd21a5843ec4a0b57
pacino9515 <1652258118@qq.com>
2017-08-28 11:13:14 +0800

    1
    1

---
+++ Fraction.java
@@ -0,0 +1,28 @@
+package one;
+
+
+class Fraction {
+	
+	
+	
+	void toDouble()
+	{ 
+		 
+	}
+	void plus(Fraction r){
+		
+	}
+	void multiply(Fraction r){
+		
+	}
+	void print(){
+		
+	}
+	
+//	public static void Fraction(int a,int b){
+//		
+//	}
+	
+
+}
+
